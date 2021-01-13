a8217206122622641e7c8cbbdb016a12a86c8978
Marcin <sommer.marcin@gmail.com>
2021-01-13 15:15:16 +0100

    baza

---
+++ charity_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CharityAppConfig(AppConfig):
+    name = 'charity_app'
---
+++ charity_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class LandingPage(View):
+    def get(self, request):
+        return render(request, "index.html")
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, "form.html")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+
+class Register(View):
+    def get(selfself, request):
+        return render(request, "register.html")
+
